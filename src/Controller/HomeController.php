5a51d30413a27c127441e845194cda14e603b31e
Layannd <brth.joana@gmail.com>
2024-10-11 23:58:57 +0200

    création ContactController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,12 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
 }
