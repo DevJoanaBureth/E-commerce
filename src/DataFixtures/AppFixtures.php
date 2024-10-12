a50c5667d867e6c9598431ee6a805ca9dd6a423e
Layannd <brth.joana@gmail.com>
2024-10-12 17:32:24 +0200

    add user BDD

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,14 +5,25 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        $product = new User();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setNom('Bureth')->setPrenom('Joana')->setEmail('brth.joana@gmail.com')->setRoles(['ROLES_USER']);
+        $encoded = $this->encoder->hashPassword($user, '123');
+        $user->setPassword($encoded);
 
+        $manager->persist($user);
         $manager->flush();
     }
 }
